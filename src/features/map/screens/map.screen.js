49ccd234d68ef4b4b536d8e3e0b8e6c40ab47ccf
EnricoGiovanno8 <genrico189@gmail.com>
2022-01-19 13:32:34 +0700

    Map Search and Screen Done

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,8 +1,19 @@
 import React, { useContext, useState, useEffect } from "react";
 import MapView from "react-native-maps";
+import styled from "styled-components";
+
+import { Search } from "../components/search.component";
+
+const Map = styled(MapView)`
+  height: 100%;
+  width: 100%;
+`;
 
 export const MapScreen = () => {
   return (
-    <MapView style={{ height: '100%' }} />
+    <>
+      <Search />
+      <Map />
+    </>
   );
 };
