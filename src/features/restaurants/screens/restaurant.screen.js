11f33132c4a9d5dba17463781dd790f0230ca3b8
EnricoGiovanno8 <genrico189@gmail.com>
2022-01-20 17:21:15 +0700

    Animations and Polish Done

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,27 +1,24 @@
 import React, { useContext, useState } from "react";
 import { TouchableOpacity } from "react-native";
-import { ActivityIndicator, Colors } from "react-native-paper";
 import styled from "styled-components/native";
+import { ActivityIndicator, Colors } from "react-native-paper";
 
-import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
-import { Spacer } from "../../../components/spacer/spacer.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
-import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
-import { Search } from "../components/search.component";
+import { FadeInView } from "../../../components/animations/fade.animation";
+import { Spacer } from "../../../components/spacer/spacer.component";
 import { FavouritesBar } from "../../../components/favourites/favourites-bar.component";
+
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import { FavouritesContext } from "../../../services/favourites/favourites.context";
 
-const RestaurantList = styled.FlatList.attrs({
-  contentContainerStyle: {
-    paddingLeft: 16,
-    paddingRight: 16,
-  },
-})``;
+import { Search } from "../components/search.component";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+
+import { RestaurantList } from "../components/restaurant-list.style";
 
 const Loading = styled(ActivityIndicator)`
   margin-left: -25px;
 `;
-
 const LoadingContainer = styled.View`
   position: absolute;
   top: 50%;
@@ -29,12 +26,17 @@ const LoadingContainer = styled.View`
 `;
 
 export const RestaurantsScreen = ({ navigation }) => {
-  const { restaurants, isLoading } = useContext(RestaurantsContext);
+  const { isLoading, restaurants } = useContext(RestaurantsContext);
   const { favourites } = useContext(FavouritesContext);
   const [isToggled, setIsToggled] = useState(false);
 
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={Colors.blue300} />
+        </LoadingContainer>
+      )}
       <Search
         isFavouritesToggled={isToggled}
         onFavouritesToggle={() => setIsToggled(!isToggled)}
@@ -45,27 +47,27 @@ export const RestaurantsScreen = ({ navigation }) => {
           onNavigate={navigation.navigate}
         />
       )}
-      {isLoading ? (
-        <LoadingContainer>
-          <Loading size={50} animating={true} color={Colors.blue300} />
-        </LoadingContainer>
-      ) : (
-        <RestaurantList
-          data={restaurants}
-          renderItem={({ item }) => (
+      <RestaurantList
+        data={restaurants}
+        renderItem={({ item }) => {
+          return (
             <TouchableOpacity
               onPress={() =>
-                navigation.push("RestaurantDetail", { restaurant: item })
+                navigation.navigate("RestaurantDetail", {
+                  restaurant: item,
+                })
               }
             >
               <Spacer position="bottom" size="large">
-                <RestaurantInfoCard restaurant={item} />
+                <FadeInView>
+                  <RestaurantInfoCard restaurant={item} />
+                </FadeInView>
               </Spacer>
             </TouchableOpacity>
-          )}
-          keyExtractor={(item, index) => index.toString()}
-        />
-      )}
+          );
+        }}
+        keyExtractor={(item) => item.name}
+      />
     </SafeArea>
   );
 };
--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -1,16 +1,29 @@
 import React from "react";
 
-import { CardStyleInterpolators, createStackNavigator } from "@react-navigation/stack";
+import {
+  CardStyleInterpolators,
+  createStackNavigator,
+} from "@react-navigation/stack";
 
-import { SettingsScreen} from "../../features/settings/screens/settings.screen"
+import { SettingsScreen } from "../../features/settings/screens/settings.screen";
+import { FavouritesScreen } from "../../features/settings/screens/favourites.screen";
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 export const SettingsNavigator = () => {
-    return (
-        <Stack.Navigator screenOptions={{ headerMode:"screen", cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid }}>
-            <Stack.Screen options={{ headerShown: false }} name="Settings" component={SettingsScreen} />
-            <Stack.Screen name="Favourites" component={() => null} />
-        </Stack.Navigator>
-    )
-}
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid,
+      }}
+    >
+      <Stack.Screen
+        options={{ headerShown: false }}
+        name="SettingsScreen"
+        component={SettingsScreen}
+      />
+      <Stack.Screen name="Favourites" component={FavouritesScreen} />
+    </Stack.Navigator>
+  );
+};
